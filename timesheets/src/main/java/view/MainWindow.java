667f86a132fcb17595e56653fef63c2c721f2e42
Aaron Traynor <github@aarontraynor.uk>
2019-07-15 15:53:52 +0100

    Create main Swing panel

---
+++ timesheets/src/main/java/view/MainWindow.java
@@ -0,0 +1,32 @@
+package view;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    private JPanel mainPanel;
+
+    public MainWindow(String name) {
+        super(name);
+        mainPanel = new JPanel();
+        init();
+    }
+
+    public MainWindow(String name, JPanel mainPanel) {
+        super(name);
+        this.mainPanel = mainPanel;
+        init();
+    }
+
+    private void init() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1280, 720);
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public void setMainPanel(JPanel mainPanel) {
+        this.mainPanel = mainPanel;
+    }
+}
