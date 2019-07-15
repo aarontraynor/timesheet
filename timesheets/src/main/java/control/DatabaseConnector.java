bef7b34bb6e7e9a9270adfe48fcc9f4ae040920c
Aaron Traynor <github@aarontraynor.uk>
2019-07-15 15:54:05 +0100

    Create main Swing panel

--- timesheets/src/main/java/control/DatabaseConnector.java
+++ timesheets/src/main/java/control/DatabaseConnector.java
@@ -1,5 +1,73 @@
 package control;
 
-public class DatabaseConnector {
+import model.Database;
 
+import java.sql.*;
+
+public class DatabaseConnector {
+    private Database db;
+    private Connection con;
+    private boolean dbConnected;
+
+    public DatabaseConnector(Database db) {
+        this.db = db;
+        dbConnected = connect();
+    }
+
+    public boolean connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jbdc:mysql://" + db.getDomain() + ":" + db.getPort() + "/timesheet", db.getUsername(), db.getPassword());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public void disconnect() {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getDatabaseUsername() {
+        return db.getUsername();
+    }
+
+    public void setDatabaseUsername(String username) {
+        db.setUsername(username);
+    }
+
+    // Get database password? Think about how to securely access this information
+
+    public void setDatabasePassword(String password) {
+        db.setPassword(password);
+    }
+
+    public String getDatabaseDomain() {
+        return db.getDomain();
+    }
+
+    public void setDatabaseDomain(String domain) {
+        db.setDomain(domain);
+    }
+
+    public int getDatabasePort() {
+        return db.getPort();
+    }
+
+    public void setDatabasePort(int port) {
+        db.setPort(port);
+    }
+
+    public boolean isConnected() {
+        return dbConnected;
+    }
 }
--- timesheets/src/main/java/view/TerminalView.java
+++ timesheets/src/main/java/view/TerminalView.java
@@ -1,5 +1,10 @@
 package view;
 
+import control.DatabaseConnector;
+import model.Database;
+
+import java.util.Scanner;
+
 /**
  * @author Aaron
  */
@@ -10,6 +15,28 @@ public class TerminalView {
      * @param args arguments given from the terminal when executing the program
      */
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String domain;
+        String username;
+        String password;
+        int port;
+
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Enter database URL: ");
+        domain = kb.nextLine();
+
+        System.out.print("Enter port number (1-65535): ");
+        port = Integer.parseInt(kb.nextLine());
+
+        System.out.print("Enter username: ");
+        username = kb.nextLine();
+
+        System.out.print("Enter password: ");
+        password = kb.nextLine();
+
+        System.out.println("Connecting...");
+        DatabaseConnector dbc = new DatabaseConnector(new Database(domain, username, password, port));
+
+        System.out.println("Connected: " + dbc.isConnected());
     }
 }
