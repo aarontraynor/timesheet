0583b0c5bef299898aa3c3c6ebeae98f964d0738
Aaron Traynor <github@aarontraynor.uk>
2019-07-15 15:43:59 +0100

    Remove protocol attribute from Database.java

--- timesheets/src/main/java/model/Database.java
+++ timesheets/src/main/java/model/Database.java
@@ -4,7 +4,6 @@ public class Database {
     private String username;
     private String password;
     private String domain;
-    private String protocol;
     private int port;
 
     /**
@@ -12,14 +11,44 @@ public class Database {
      * @param username the username used to connect to the database
      * @param password the password of the associated username
      * @param domain the URL of the database
-     * @param protocol the protocol used to access the database
      * @param port the port used to access the database
      */
-    public Database(String username, String password, String domain, String protocol, int port) {
+    public Database(String username, String password, String domain, int port) {
         this.username = username;
         this.password = password;
         this.domain = domain;
-        this.protocol = protocol;
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
         this.port = port;
     }
 }
